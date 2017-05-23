2ecb340ae08a93cc93b89f0e186664114c541621
Ali Ariff <ali.ariff12@gmail.com>
2017-05-23 16:31:09 +0200

    pretty html

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -9,14 +9,21 @@ module.exports = function (grunt) {
       },
       target: ['*.js', 'public/app/**/*.js'],
     },
+    prettify: {
+      all: {
+        expand: true,
+        src: ['public/**/*.html'],
+      },
+    },
   });
 
   // Load NPM tasks
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-prettify');
 
   // Making grunt default to force in order not to break the project.
   grunt.option('force', true);
 
   // Default task(s).
-  grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('default', ['eslint', 'prettify']);
 };
--- frontend/index.js
+++ frontend/index.js
@@ -12,5 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
 app.listen(port);
 console.log(`App listening on port ${port}`);
