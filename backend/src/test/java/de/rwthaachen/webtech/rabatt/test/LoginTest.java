9140001fb81d02a5a79a8f21de7aa07a87058991
Ali Ariff <ali.ariff12@gmail.com>
2017-06-21 19:01:36 +0200

    init unit testing

---
+++ backend/src/test/java/de/rwthaachen/webtech/rabatt/test/LoginTest.java
@@ -0,0 +1,12 @@
+package de.rwthaachen.webtech.rabatt.test;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTest {
+  @Test
+  public void testPrintHelloWorld() {
+    Assert.assertEquals("a", "Hello World");
+  }
+}
