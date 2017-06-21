bee01166f8e1a4eaa99cba9869a9fc0d357852f8
Ali Ariff <ali.ariff12@gmail.com>
2017-06-22 00:37:14 +0200

    discount repo test

--- backend/src/test/java/de/rwthaachen/webtech/rabatt/test/DiscountCRUDTest.java
+++ backend/src/test/java/de/rwthaachen/webtech/rabatt/test/DiscountCRUDTest.java
@@ -1,5 +1,115 @@
 package de.rwthaachen.webtech.rabatt.test;
 
-public class DiscountCRUDTest {
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import de.rwthaachen.webtech.rabatt.config.AuthoritiesConstants;
+import de.rwthaachen.webtech.rabatt.config.DispatcherServletInitializer;
+import de.rwthaachen.webtech.rabatt.config.WebConfig;
+import de.rwthaachen.webtech.rabatt.config.WebSecurityConfig;
+import de.rwthaachen.webtech.rabatt.model.Discount;
+import de.rwthaachen.webtech.rabatt.model.User;
+import de.rwthaachen.webtech.rabatt.repository.DiscountRepository;
+import de.rwthaachen.webtech.rabatt.repository.UserRepository;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    classes = {WebConfig.class, WebSecurityConfig.class, DispatcherServletInitializer.class})
+public class DiscountCRUDTest extends JPAHibernateTest {
+  @Autowired
+  private DiscountRepository discountRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Test
+  public void findByCreator() {
+    create();
+    User user = userRepository.findAll().get(0);
+    Discount discount = discountRepository.findByCreator(user).get(0);
+    assertNotNull(discount);
+  }
+
+  @Test
+  public void findByTitle() {
+    create();
+    Discount discount = discountRepository.findByTitle("Half price coca-cola").get(0);
+    assertNotNull(discount);
+  }
+
+  @Test
+  public void findAll() {
+    create();
+    List<Discount> discounts = discountRepository.findAll();
+    assertEquals(1, discounts.size());
+  }
+
+  @Test
+  public void create() {
+    Date today = new Date();
+    Calendar c = Calendar.getInstance();
+    c.setTime(today);
+    c.add(Calendar.DATE, 1);
+    Date tomorrow = c.getTime();
+
+    User testUser = new User();
+    testUser.setUsername("testUser");
+    testUser.setPassword("testUser");
+    testUser.setEmail("testUser@gmail.com");
+    testUser.setRole(AuthoritiesConstants.ADMIN);
+    testUser.setEnabled(true);
+    userRepository.save(testUser);
+
+    Discount netto = new Discount();
+    netto.setTitle("Half price coca-cola");
+    netto.setValidFrom(today);
+    netto.setValidUntil(tomorrow);
+    netto.setCategory("Food");
+    netto.setLink("http://google.com");
+    netto.setCreator(testUser);
+    discountRepository.save(netto);
+
+    Discount discount = discountRepository.findByCreator(testUser).get(0);
+    assertNotNull(discount);
+
+    List<Discount> discounts = discountRepository.findAll();
+    assertEquals(1, discounts.size());
+  }
+
+  @Test
+  public void update() {
+    create();
+    Discount discount = discountRepository.findAll().get(0);
+    assertEquals("Half price coca-cola", discount.getTitle());
+    discount.setTitle("pepsi");
+
+    discountRepository.save(discount);
+
+    discount = discountRepository.findAll().get(0);
+    assertEquals("pepsi", discount.getTitle());
+  }
+
+  @Test
+  public void delete() {
+    create();
+    List<Discount> discounts = discountRepository.findAll();
+    assertEquals(1, discounts.size());
+
+    discountRepository.deleteAll();
+
+    discounts = discountRepository.findAll();
+    assertEquals(0, discounts.size());
+  }
 }
